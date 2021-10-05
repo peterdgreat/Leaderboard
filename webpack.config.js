ca80e82efbad86ec717ce626070a37fe64de89b6
Ajayi Peter <peterdgreat2016@gmail.com>
2021-10-05 23:11:05 +0200

    Webpack running

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const Htmlwebpack = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'production',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new Htmlwebpack({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
